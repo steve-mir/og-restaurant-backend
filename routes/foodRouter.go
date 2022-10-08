b4e7b0fef3d50d6fab33839c9819026e49ee8558
steve-mir <ekechukwuemeka25@gmail.com>
2022-10-08 17:36:12 +0100

    Added User and food routes

--- routes/foodRouter.go
+++ routes/foodRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/steve-mir/og-restaurant-backend/controllers"
+)
+
+func FoodRoutes(route *gin.Engine) {
+	route.GET("/foods", controllers.GetFoods())
+	route.GET("/foods/:id", controllers.GetFood())
+	route.POST("/foods", controllers.CreateFood())
+	route.PATCH("foods/:id", controllers.UpdateFood())
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/steve-mir/og-restaurant-backend/controllers"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/users", controllers.GetUser())
+	incomingRoutes.GET("/users/:id", controllers.GetUser())
+	incomingRoutes.POST("/users/register", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+}
